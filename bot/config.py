c28f03a52a40f2031933bfd69e19d57ff251ff99
WatashiKun <152046420+WatashiKun@users.noreply.github.com>
2023-11-26 11:43:50 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,18 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("APP_ID", "3847632")
+    API_HASH = config("API_HASH", "1a9708f807ddd06b10337f2091c67657")
+    BOT_TOKEN = config("BOT_TOKEN", "6200331513:AAFcjwJdhu9zt4xje1MWod1LhvkXhHJ_3cM")
     DEV = 1322549723
-    OWNER = config("OWNER")
-    FFMPEG = config(
-        "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
-    )
-    THUMB = config(
-        "THUMBNAIL", default="https://graph.org/file/75ee20ec8d8c8bba84f02.jpg"
-    )
+    OWNER = config("OWNER", "5385471287")
+    FFMPEG = config("FFMPEG", "ffmpeg ""{}""-i -preset veryfast -c:v libx265 -crf 27 -s 1280x720 -map 0:v -c:a aac ""{}""") 
+    THUMB = config("THUMBNAIL", "https://graph.org/file/75ee20ec8d8c8bba84f02")
 except Exception as e:
     print("Environment vars Missing")
     print("something went wrong")
